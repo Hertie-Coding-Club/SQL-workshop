04b28ba479bc3ea1cef3b408b9a211d0811670a3
Minho Kang <rkdalsgh32@gmail.com>
2024-04-10 11:55:08 +0200

    Add Join Part

--- Advanced_SQL.R
+++ Advanced_SQL.R
@@ -19,3 +19,39 @@ SELECT * FROM EPEX_12_20_12_13 WHERE PRICE > (SELECT AVG(PRICE) FROM EPEX_12_20_
 SELECT * FROM ABOVE_AVG"
 
 dbGetQuery(con, query_with)
+
+# Now we turn to the other topic: JOIN
+
+## Inner Join
+
+query_inner <- "SELECT * FROM EPEX_12_20_12_13 AS A 
+INNER JOIN EPEX_USER AS B ON A.ID = B.ID"
+dbGetQuery(con, query_inner)
+
+query_inner_bad <- "SELECT EPEX_12_20_12_13.QUANTITY 
+FROM EPEX_12_20_12_13 
+INNER JOIN EPEX_USER 
+ON EPEX_12_20_12_13.ID = EPEX_USER.ID "
+
+dbGetQuery(con, query_inner_bad)
+
+query_inner_good <- "SELECT A.QUANTITY 
+FROM EPEX_12_20_12_13 AS A 
+INNER JOIN EPEX_USER AS B 
+ON A.ID = B.ID  "
+
+dbGetQuery(con, query_inner_good)
+
+## Left Join 
+
+query_left <- "SELECT * 
+FROM EPEX_12_20_12_13 AS A 
+LEFT JOIN EPEX_USER AS B ON A.ID = B.ID"
+dbGetQuery(con, query_left)
+
+query_left_name <- "SELECT A.ID, B.NAME 
+FROM EPEX_12_20_12_13 AS A 
+LEFT JOIN EPEX_USER AS B 
+ON A.ID = B.ID "
+
+dbGetQuery(con, query_left_name)
