4642be9cb67ca07a0f51bc23050a02e418cd2bac
bprtdaniel <daniel@boppert.net>
2024-04-10 10:51:06 +0200

    basic SQL syntax exercises

--- Basic_SQL_solutions.R
+++ Basic_SQL_solutions.R
@@ -5,35 +5,34 @@ con <- dbConnect(SQLite(), dbname="data/trades.sqlite")
 dbListTables(con)
 
 # Select all rows from the table "epex_12_20_12_13"
-dbGetQuery(con, "")
+dbGetQuery(con, "SELECT * FROM epex_12_20_12_13")
 
 # Calculate average price 
-dbGetQuery(con, "")
+dbGetQuery(con, "SELECT AVG(price) FROM epex_12_20_12_13")
 
 # WHERE conditions
 
 # Select only trades with a price greater than 10
-dbGetQuery(con, "")
+dbGetQuery(con, "SELECT id FROM epex_12_20_12_13 WHERE price > 10")
 
 # Select only trades with a price greater than 10, on the buy side
-dbGetQuery(con, "")
+dbGetQuery(con, "SELECT id FROM epex_12_20_12_13 WHERE price > 10 AND side = 'buy'")
 
 
 # How many trades have a quantity below 15?
-dbGetQuery(con, "")
+dbGetQuery(con, "SELECT COUNT(id) FROM epex_12_20_12_13 WHERE quantity < 15")
 
 
 # Use "group by" function and aggregate functions
 
 # What's the average price for trades that follow strategy1?
-dbGetQuery(con, "")
+dbGetQuery(con, "SELECT AVG(price) FROM epex_12_20_12_13 WHERE strategy = strategy_1")
 
 # What is the highest price on a trade, on the sell side, with a quantity of over 13? Which one is it?
-dbGetQuery(con, "")
+dbGetQuery(con, "SELECT id, MAX(price) FROM epex_12_20_12_13 WHERE side = 'sell' AND quantity > 13")
 
 # If the query length is getting longer, you can type it separately.
 
 epex_12_20_12_13query <- "SELECT STRATEGY, SUM(QUANTITY) FROM EPEX_12_20_12_13 
 WHERE SIDE = 'buy' GROUP BY STRATEGY"
 dbGetQuery(con, query)
-
